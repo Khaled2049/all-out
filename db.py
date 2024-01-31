ed7587888f777d243487ed63179622126c9a7df6
khaled2049 <khaledhossain.not@gmail.com>
2024-01-31 08:21:28 -0700

    Clean up

--- db.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-
-# climbs = [{'route_name': 'Wheres Waldo?', 'grade': {'YDS': 'V2', 'Font': '5+'}, 'safety': '', 'type': {'boulder': True}, 'fa': 'unknown', 'description': ['Sit Start on the crack. Pull a big move to a crimp and work across the face to the arete on the left side of the boulder. Top out from there. Super cool movement for an easy problem.'], 'location': '', 'protection': ['Pads'], 'metadata': {'left_right_seq': '999999', 'parent_lnglat': [-91.54207, 42.02717], 'parent_sector': "Waldo's Rock Park", 'mp_route_id': '118170758', 'mp_sector_id': '118170033'}}]
-
