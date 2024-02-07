444bc746b7906844426f84f138ce3259ae0d8128
Khaled <khaledhossain.not@gmail.com>
2024-02-07 11:01:03 -0700

    Fly to position

--- allOut/src/screens/MapScreen.js
+++ allOut/src/screens/MapScreen.js
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import Mapbox, { MarkerView } from "@rnmapbox/maps";
+import React, { useRef, useEffect, useState } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+import MapboxGL, { MarkerView } from "@rnmapbox/maps";
 import { useRoute } from "@react-navigation/native";
 
-Mapbox.setAccessToken(
+MapboxGL.setAccessToken(
   "pk.eyJ1Ijoia2hhbGVkMjA0OCIsImEiOiJjbHJqbnI2azQwNWRyMmtraXlzdWR3N2xoIn0.25oYJMrELC1s9VPPA60ndA"
 );
 
 const MapScreen = () => {
+  const mapRef = useRef(null);
+  const cameraRef = useRef(null);
   const route = useRoute();
   const { selectedTrail } = route.params || {};
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (mapRef.current && cameraRef.current) {
+      console.log("Setting camera");
+      cameraRef.current.setCamera({
+        centerCoordinate: selectedTrail
+          ? selectedTrail.geometry.coordinates
+          : [39.5501, 105.7821],
+        zoomLevel: 7,
+        animationMode: "flyTo",
+        animationDuration: 3000, // Adjust the duration as needed
+      });
+    }
+
+    setIsLoading(false);
+  }, [selectedTrail, mapRef, cameraRef]);
+
+  if (isLoading) {
+    // Show a loading screen
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="blue" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map}>
+        <MapboxGL.MapView ref={mapRef} style={styles.map}>
+          <MapboxGL.Camera ref={cameraRef} />
           {selectedTrail && (
             <MarkerView
+              id="marker"
               coordinate={selectedTrail.geometry.coordinates}
-              title={selectedTrail.properties.name}
-            />
+            >
+              <View style={styles.marker} />
+            </MarkerView>
           )}
-        </Mapbox.MapView>
+        </MapboxGL.MapView>
       </View>
     </View>
   );
@@ -36,9 +68,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   container: {
-    ...StyleSheet.absoluteFillObject, // This makes the container take up the entire screen
+    ...StyleSheet.absoluteFillObject,
   },
   map: {
     flex: 1,
+  },
+  marker: {
+    width: 20,
+    height: 20,
+    backgroundColor: "red",
+    borderRadius: 10,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
